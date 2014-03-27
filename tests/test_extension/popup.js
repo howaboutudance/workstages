252770f4c9d663a81c5ca60745ff85b8bdc84e32
Michael Penhallegon <crimson@crimsonfae.net>
2014-03-26 19:53:37 -0700

    intial try at the test extension to start up

--- tests/test_extension/popup.js
+++ tests/test_extension/popup.js
@@ -1,11 +1,26 @@
+var INTERVAL = 25;
+var url='http://0.0.0.0:8080/test/';
+testdate = 1395005507277;
 $(document).ready( function () {
-  console.log("got into script");
-  $("#basic").submit(function(ev) {
-    var url = '0.0.0.0:8080/test';
-   $.getJSON("http://0.0.0.0:8080/test", function(data) {
-     $('#result').text(data.message);
-     }
-    );
-   ev.preventDefault();
+  $("#start").click(function(ev) {
+    startTimeStamp = Date.now();
+    data = {
+      "interval":INTERVAL,
+      "startTimeStamp":startTimeStamp
+    };
+    $.post(url,data, function() {$('#result').text("start request success");});
+  });
+  $("#retrieve").click(function(){
+    
+    timenow = Date.now;
+    $.ajax({
+      type:"GET",
+      url: url,
+      dataType: 'json',
+      success: function(data) {
+        endtime = new Date(data.endTime)
+       $('#result').text("current stage ends at: "+endtime.toLocaleTimeString());
+      }
+    });
   });
 });
