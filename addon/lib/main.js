f9ad8205a7edb501d083d2ac6b8c3857de174122
Michael Penhallegon <crimson@crimsonfae.net>
2014-07-30 14:10:10 -0700

    made addon icon change red on click, useful for starting a stage

--- addon/lib/main.js
+++ addon/lib/main.js
@@ -1,17 +1,28 @@
-var buttons = require('sdk/ui/button/action');
+var { ToggleButton } = require('sdk/ui/button/toggle');
+var work_state = 0; /* sets the state for the button: 0 for inactive, 1 for
+                    * active and 2 for break
+                    */
 var tabs = require("sdk/tabs");
 
-var button = buttons.ActionButton({
-  id: "mozilla-link",
-  label: "Visit Mozilla",
+var button = ToggleButton({
+  id: "workstage-button",
+  label: "Start workstage",
   icon: {
     "16": "./icon-16.png",
     "32": "./icon-32.png",
     "64": "./icon-64.png"
   },
-  onClick: handleClick
+  onChange: handleClick
 });
 
 function handleClick(state) {
-  tabs.open("http://www.mozilla.org/");
+  if (work_state == 0) {
+    button.state("window", {
+      icon : {
+        "16": "./icon-active-16.png",
+        "32": "./icon-active-32.png",
+        "64": "./icon-active-64.png"
+      }
+    });
+  }
 }
