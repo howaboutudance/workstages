b7bc3b618b35e1d0dc298a13b03df4368475d9f6
Michael Penhallegon <crimson@crimsonfae.net>
2014-07-30 14:40:41 -0700

    finish up basic logic for stage add-on now can time a session

--- addon/lib/main.js
+++ addon/lib/main.js
@@ -1,28 +1,66 @@
+/* (c) 2014 Michael Penhallegon
+ */
+
+// import libs
 var { ToggleButton } = require('sdk/ui/button/toggle');
+var timer = require("sdk/timers");
+var tabs = require("sdk/tabs");
+
+// setup work_state hold state of which work is at, set time length
 var work_state = 0; /* sets the state for the button: 0 for inactive, 1 for
                     * active and 2 for break
                     */
-var tabs = require("sdk/tabs");
+var timer_length_work= 15 * 1000; // set to a default of 20 minutes
+var timer_length_break= 5 * 1000; // set to a default of 20 minutes
+var icon_active_prefix = "icon-active-";
+var icon_inactive_prefix = "icon-";
+var icon_break_prefix = "icon-break-";
 
+//creats button object
 var button = ToggleButton({
   id: "workstage-button",
   label: "Start workstage",
   icon: {
-    "16": "./icon-16.png",
-    "32": "./icon-32.png",
-    "64": "./icon-64.png"
+    "16": "./"+icon_inactive_prefix+"16.png",
+    "32": "./"+icon_inactive_prefix+"32.png",
+    "64": "./"+icon_inactive_prefix+"64.png"
   },
-  onChange: handleClick
+  onChange: handleChange
 });
 
-function handleClick(state) {
+//handler for change
+function handleChange(state) {
   if (work_state == 0) {
     button.state("window", {
       icon : {
-        "16": "./icon-active-16.png",
-        "32": "./icon-active-32.png",
-        "64": "./icon-active-64.png"
+        "16": "./"+icon_active_prefix+"16.png",
+        "32": "./"+icon_active_prefix+"32.png",
+        "64": "./"+icon_active_prefix+"64.png"
       }
     });
+    work_state = 1;
+    timer.setInterval(handleStageEnd, timer_length_work);
+  } if (work_state == 2) {
+    button.state("window", {
+      icon : {
+        "16": "./"+icon_break_prefix+"16.png",
+        "32": "./"+icon_break_prefix+"32.png",
+        "64": "./"+icon_break_prefix+"64.png"
+      }
+    });
+    work_state = 0;
+    timer.setInterval(handleStageEnd, timer_length_break);
   }
 }
+
+function handleStageEnd() {
+  button.state("window",{
+    icon : {
+        "16": "./"+icon_inactive_prefix+"16.png",
+        "32": "./"+icon_inactive_prefix+"32.png",
+        "64": "./"+icon_inactive_prefix+"64.png"
+    }
+  });
+  work_state = 2;
+}
+
