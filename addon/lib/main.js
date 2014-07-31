7b92fbcb2c134d1979b82d3df771ac74884abab7
Michael Penhallegon <crimson@crimsonfae.net>
2014-07-31 00:24:10 -0700

    added notifications and panel that will show when clicked

--- addon/lib/main.js
+++ addon/lib/main.js
@@ -2,19 +2,26 @@
  */
 
 // import libs
-var { ToggleButton } = require('sdk/ui/button/toggle');
-var timer = require("sdk/timers");
+var notificatons = require("sdk/notifications");
+var panel = require("sdk/panel");
 var tabs = require("sdk/tabs");
+var shelf = require("sdk/shelf");
+var timer = require("sdk/timers");
+var { ToggleButton } = require('sdk/ui/button/toggle');
 
 // setup work_state hold state of which work is at, set time length
+
 var work_state = 0; /* sets the state for the button: 0 for inactive, 1 for
-                    * active and 2 for break
+                    *  active and 2 for break
                     */
+
 var timer_length_work= 25 * 60 * 1000; // set to a default of 20 minutes
-var timer_length_break= 5 * 60 * 1000; // set to a default of 20 minutes
+var timer_length_break= 5 * 60 * 1000; // set to a default of 5 minutes
 var icon_active_prefix = "icon-active-";
 var icon_inactive_prefix = "icon-";
 var icon_break_prefix = "icon-break-";
+
+/*     Objects     */
 
 //creats button object
 var button = ToggleButton({
@@ -28,8 +35,26 @@ var button = ToggleButton({
   onChange: handleChange
 });
 
+//create a panel
+
+var panel = panels.Panel({
+  contentURL: self.data.url("panel.html");
+  onHide: handleHide
+});
+
+
+/*    Handlers    */
+
 //handler for change
 function handleChange(state) {
+
+  // if checked show panel
+  if(state.checked) {
+    panel.show({
+      position:button
+    });
+
+  // if statements to handle which color to show button 
   if (work_state == 0) {
     button.state("window", {
       icon : {
@@ -49,10 +74,15 @@ function handleChange(state) {
       }
     });
     work_state = 0;
+    notifications.notify({
+      title:"Break Done!",
+      text: "your break is done return to your browser to start a new workstage",
+    });
     timer.setInterval(handleStageEnd, timer_length_break);
   }
 }
 
+//triggered when workstage ends
 function handleStageEnd() {
   button.state("window",{
     icon : {
@@ -62,5 +92,15 @@ function handleStageEnd() {
     }
   });
   work_state = 2;
+  notfications.notify({
+    title: "Stage Done!",
+    text: "The workstage you were is done! click the icon again to go on a 5 minute break",
+  });
+
 }
 
+// handle panel hiding
+
+function handleHide() {
+  button.state('window', {checked:false});
+}
