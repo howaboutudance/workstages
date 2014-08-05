6f78cd5bc0b94898d99dd0643b3407f3d3603359
Michael Penhallegon <crimson@crimsonfae.net>
2014-08-05 16:33:08 -0700

    addon does basic push request when starting stage

--- addon/lib/main.js
+++ addon/lib/main.js
@@ -2,13 +2,14 @@
  */
 
 // import libs
-var notificatons = require("sdk/notifications");
+var notifications = require("sdk/notifications");
 var tabs = require("sdk/tabs");
 var Request = require("sdk/request").Request;
 var self = require("sdk/self");
-var panel = require("sdk/panel");
+var panels = require("sdk/panel");
+var request = require("sdk/request");
 var timer = require("sdk/timers");
-var { ToggleButton } = require('sdk/ui/button/toggle');
+var { ActionButton } = require('sdk/ui/button/action');
 
 // setup work_state hold state of which work is at, set time length
 
@@ -16,8 +17,8 @@ var work_state = 0; /* sets the state for the button: 0 for inactive, 1 for
                     *  active and 2 for break
                     */
 
-var timer_length_work= 25 * 60 * 1000; // set to a default of 20 minutes
-var timer_length_break= 5 * 60 * 1000; // set to a default of 5 minutes
+var timer_length_work= .5 * 60 * 1000; // set to a default of 20 minutes
+var timer_length_break= .1 * 60 * 1000; // set to a default of 5 minutes
 var icon_active_prefix = "icon-active-";
 var icon_inactive_prefix = "icon-";
 var icon_break_prefix = "icon-break-";
@@ -25,7 +26,7 @@ var icon_break_prefix = "icon-break-";
 /*     Objects     */
 
 //creats button object
-var button = ToggleButton({
+var button = ActionButton({
   id: "workstage-button",
   label: "Start workstage",
   icon: {
@@ -33,23 +34,13 @@ var button = ToggleButton({
     "32": "./"+icon_inactive_prefix+"32.png",
     "64": "./"+icon_inactive_prefix+"64.png"
   },
-  onChange: handleChange
+  onClick: handleClick
 });
-
-//create a panel
-
-// TODO: properly setup code here
 
 /*    Handlers    */
 
 //handler for change
-function handleChange(state) {
-
-  // if checked show panel
-  if(state.checked) {
-    console.log("panel should be shown TODO");// TODO: need to set up to show
-                                              // panel
-  }
+function handleClick(state) {
 
   // if statements to handle which color to show button 
   if (work_state == 0) {
@@ -62,7 +53,11 @@ function handleChange(state) {
     });
     work_state = 1;
     timer.setInterval(handleStageEnd, timer_length_work);
-    startWorkStage(1);
+    startWorkStage(work_state);
+    notifications.notify({
+      title:"Stage Started!",
+      text: ""
+    });
   } if (work_state == 2) {
     button.state("window", {
       icon : {
@@ -71,12 +66,12 @@ function handleChange(state) {
         "64": "./"+icon_break_prefix+"64.png"
       }
     });
+    timer.setInterval(handleStageEnd, timer_length_break);
     work_state = 0;
     notifications.notify({
       title:"Break Done!",
-      text: "your break is done return to your browser to start a new workstage",
+      text: "your break is done return to your browser to start a new workstage"
     });
-    timer.setInterval(handleStageEnd, timer_length_break);
   }
 }
 
@@ -89,21 +84,32 @@ function handleStageEnd() {
         "64": "./"+icon_inactive_prefix+"64.png"
     }
   });
-  work_state = 2;
-  notfications.notify({
-    title: "Stage Done!",
-    text: "The workstage you were is done! click the icon again to go on a 5 minute break",
-  });
+  if (work_state == 1) {
+    work_state = 2;
+    notifications.notify({
+      title: "Stage Done!",
+      text: "The workstage you were is done! click the icon again to go on a 5 minute break",
+    });
+  }
 
-}
-
-// handle panel hiding
-
-function handleHide() {
-  button.state('window', {checked:false});
 }
 
 //send request to create work stage/break stage
-function startWorkStage(type) {
-  console.log(Date.now());
+function startWorkStage(stage_type) {
+  var stage_length = 0;
+  if (stage_type == "work"){
+    stage_length = timer_length_work/6000;
+  } if (stage_type == "break") {
+    stage_length == timer_length_break/6000;
+  }
+  var newStage = Request({
+    url:"http://localhost:8080/latest/",
+    content: {startTimeStamp: Date.now(),
+      type:stage_type,
+      interval:stage_length
+    },
+    contentType: "application/json"
+  }).post();
+
 }
+
